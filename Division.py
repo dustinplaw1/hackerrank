bbc5d0793c830cbd95250048b1613903d1861698
Dustin Law <dpl656@mail.usask.ca>
2022-02-23 18:25:16 -0600

    Initial Commit

---
+++ Division.py
@@ -0,0 +1,20 @@
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+#
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+#
+# No rounding or formatting is necessary.
+#
+# Example
+#
+#
+# The result of the integer division .
+# The result of the float division is
+
+
+
+a = 3
+b = 5
+
+print (a//b)
+print(a/b)
